f5960522e5b15c810b0f7f7bda5d46fffbd774d1
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2020-03-25 12:42:09 +0100

    Kötél és leszármazottai

---
+++ src/sumatra/NoRope.java
@@ -0,0 +1,17 @@
+package sumatra;
+
+/**
+ * Kötél leszármazott, azt reprezentálja, hogy egy játékosnak nincsen kötele.
+ */
+public class NoRope extends Rope {
+    /**
+     * Játékosmentő függvény, a NoRope esetében viszont nem történik semmi, mert ezzel a "kötéllel"
+     * nem lehet játékost menteni.
+     * @param p A kimentendő játékos.
+     * @param t A kimentendő játékos mezője.
+     * @param newTile A kötelet birtokló játékos mezője.
+     */
+    @Override
+    public void save(Player p, Tile t, Tile newTile) { }
+
+}
