aacf675dcf829e457908054a76abc0ecc6a922f3
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2020-04-17 16:33:57 +0200

    Lett interpreter váz is, használjátok egészséggel

---
+++ src/sumatra/Interpreter.java
@@ -0,0 +1,19 @@
+package sumatra;
+
+/**
+ * Parancsértelmező osztály. A függvényei statikusak, kapnak egy-egy parancs sort, és értelmezik azt.
+ */
+public class Interpreter {
+
+    public boolean interpretPlayerCommand(String cmd) {
+        // TODO
+    }
+
+    public boolean interpretGameplayCommand(String cmd) {
+        // TODO
+    }
+
+    public boolean interpretBasicCommand(String cmd) {
+        // TODO
+    }
+}
