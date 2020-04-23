cd6a8ca7d20265d78463a22d2ebf7ee9eea97a99
Kacso Peter <kacso.pepe@yahoo.com>
2020-04-23 19:59:45 +0300

    Bear kommentek.

--- src/sumatra/Bear.java
+++ src/sumatra/Bear.java
@@ -13,6 +13,9 @@ public class Bear extends Creature{
         type = "Polar bear";
     }
 
+    /**
+     * Null-t ad vissza, ezt a játék megnyerése megállapitásánál kell használni, amibe a medve nem számít bele.
+     */
     @Override
     public Tile getTile() { 
         return null; 
@@ -30,7 +33,9 @@ public class Bear extends Creature{
         pw.println(prefix + index + " polarbear " + World.getInstance().getTileIndex(tile));
         pw.flush();
     }
-
+    /**
+     * A medve lejátsza a körét, feltételezve, hogy a játék automata módban van.
+     */
     @Override
     void playRound() {       
         // Move in random direction.
@@ -39,6 +44,9 @@ public class Bear extends Creature{
         move( neighbours.get( targetTileIdx ) );
     }
 
+    /**
+     * A medve lejátsza a körét, feltételezve, hogy a játék managed módban van.
+     */
     @Override
     void playManagedRound() {
         Scanner input = World.getInstance().getInputScanner();
@@ -47,20 +55,35 @@ public class Bear extends Creature{
         move(World.getInstance().getTileAt(Integer.parseInt(line)));
     }
 
+    /**
+     * Medveként ütközik egy másik lénnyel.
+     * @param c a másik lény.
+     */
     @Override
     void collideWith(Creature c) {
         c.hitBy(this);
     }
 
+    /** A medve beleesik a vízbe. Mivel a medvének a víz nem árt, nem történik semmi.*/
     @Override
     void fallInWater() {}
 
+    /** 
+     * A medvét megpróbálja sebezni valami (hóvihar), de a medvét ez nem zavarja, tehát nem történik semmi.
+     * @param amount ez most nem csinál semmit, mert nem sebződik a medve. 
+     */
     @Override
     void damage(int amount) {}
 
+    /**
+     * A medvének nekimegy egy másik, medve, nem történik semmi.
+     */
     @Override
     void hitBy(Bear b) { }
 
+    /**
+     * Egy játékos azt akarja, hogy a medve mentse meg őt. A medve nem csinál ilyet, tehát nem történik semmi.
+     */
     @Override
     boolean saveMe(Player p, Tile t) { 
         return false; 
