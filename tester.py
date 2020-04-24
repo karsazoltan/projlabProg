ed1027212cd7668580c55207b4208614f6a67a40
Imetomi <imetstamas@gmail.com>
2020-04-24 18:34:49 +0300

    God help us all.

--- tester.py
+++ tester.py
@@ -3,6 +3,8 @@ import subprocess
 from subprocess import PIPE, STDOUT
 import time
 import sys
+from threading import Thread
+import threading
 
 
 succesful = 0
@@ -13,7 +15,7 @@ tests = os.listdir("tesztek/")
 full_path = os.path.abspath(os.getcwd())
 directories = []
 start = 0
-end = 44
+end = 4
 
 
 tests = sorted(tests, key = lambda x:int(x[:2]))
@@ -24,23 +26,25 @@ for directory in tests:
 for directory in directories[start:end + 1]:
     index = directory[:2].strip(' ')
     stdout_ouptut = directory[-1] == '1'
-    
-    cmd = open(os.path.join(full_path, test_folder, directory, index + '-cmd.txt'), 'r')
-    p = subprocess.Popen('java -jar projlabProg.jar', stdin=PIPE, stdout=PIPE, stderr=STDOUT)
 
+    path = os.path.join(full_path, test_folder, directory, index + '-cmd.txt')
+    cmd = open(path, 'r')
+    p = subprocess.Popen('java -jar projlabProg.jar', stdin=PIPE, stdout=PIPE, stderr=STDOUT)
     for command in cmd.readlines():
         if 'load' in command:
             command = 'load ' + test_folder + '\\' + directory + '\\' + index + '-in.txt'
         elif 'save' in command:
             command = 'save ' + test_folder + '\\' + directory + '\\' + index + '-out.txt'
+        time.sleep(0.01)
         p.stdin.write(bytes(command + '\n', 'utf-8'))
-    p.stdin.write(bytes('exit\n', 'utf-8'))
     cmd.close()
-
 
     okay = True
     ep = os.path.join(full_path, test_folder, directory, index + '-expected.txt')
     gp = os.path.join(full_path, test_folder, directory, index + '-out.txt')
+
+
+
 
     with open(ep, 'r') as expected, open(gp, 'r') as generated:    
         expected_lines = [line for line in expected]
@@ -59,11 +63,12 @@ for directory in directories[start:end + 1]:
                     okay = False
                     break
 
+
     if okay:
         succesful += 1
         print('[INFO] Test ' + index + '. succeeded.')
     else:
-        print('[INFO] Test ' + index + '. failed.')    
+        print('[WARNING] Test ' + index + '. failed.')    
 
 if succesful == end - start + 1:
     print('[DONE] Every test was succesful.')
