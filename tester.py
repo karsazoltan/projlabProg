0a04de4c0c25c42e5d9e2e148ac503fc09dbebf5
Imetomi <imetstamas@gmail.com>
2020-04-24 16:23:01 +0300

    Dolgozom a pythonon csak nem egyszeru

--- tester.py
+++ tester.py
@@ -1,7 +1,9 @@
 import os
 import subprocess
-from subprocess import PIPE
+from subprocess import PIPE, STDOUT
 import time
+import sys
+
 
 succesful = 0
 nr = 44
@@ -10,37 +12,37 @@ test_folder = 'tesztek'
 tests = os.listdir("tesztek/")
 full_path = os.path.abspath(os.getcwd())
 directories = []
+start = 0
+end = 44
 
 
+tests = sorted(tests, key = lambda x:int(x[:2]))
 for directory in tests:
     directories.append(directory)
 
 
-for directory in directories:
+for directory in directories[start:end + 1]:
     index = directory[:2].strip(' ')
     stdout_ouptut = directory[-1] == '1'
     
     cmd = open(os.path.join(full_path, test_folder, directory, index + '-cmd.txt'), 'r')
-    p = subprocess.Popen('java -jar projlabProg.jar', stdin=PIPE, stdout=PIPE)
+    p = subprocess.Popen('java -jar projlabProg.jar', stdin=PIPE, stdout=PIPE, stderr=STDOUT)
 
     for command in cmd.readlines():
         if 'load' in command:
-            command = 'load ' + os.path.join(full_path, test_folder, directory, index + '-in.txt')
-        if 'save' in command:
-            command = 'save ' + '"' + os.path.join(full_path, test_folder, directory, index + '-out.txt') + '"'
-        print(command)
+            command = 'load ' + test_folder + '\\' + directory + '\\' + index + '-in.txt'
+        elif 'save' in command:
+            command = 'save ' + test_folder + '\\' + directory + '\\' + index + '-out.txt'
         p.stdin.write(bytes(command + '\n', 'utf-8'))
+    p.stdin.write(bytes('exit\n', 'utf-8'))
     cmd.close()
 
-    
-    generated = None 
-    if stdout_ouptut:
-        generated = p.stdout
-    else:
-        generated = open(os.path.join(full_path, test_folder, directory, index + '-out.txt'), 'r')
 
     okay = True
-    with open(os.path.join(full_path, test_folder, directory, index + '-expected.txt'), 'r') as expected:
+    ep = os.path.join(full_path, test_folder, directory, index + '-expected.txt')
+    gp = os.path.join(full_path, test_folder, directory, index + '-out.txt')
+
+    with open(ep, 'r') as expected, open(gp, 'r') as generated:    
         expected_lines = [line for line in expected]
         generated_lines = [line for line in generated]
 
@@ -49,18 +51,21 @@ for directory in directories:
         
         if okay:
             for e, g in zip(expected_lines, generated_lines):
-                if e != g:
+                e = e.strip('\n').replace(' ', '')
+                g = g.strip('\n').replace(' ', '')
+                if sorted(e) != sorted(g):
+                    print(e)
+                    print(g)
                     okay = False
                     break
 
     if okay:
         succesful += 1
-        print('[INFO] Test Nr. ' + index + ' succeeded.')
+        print('[INFO] Test ' + index + '. succeeded.')
     else:
-        print('[INFO] Test Nr. ' + index + ' failed.')    
+        print('[INFO] Test ' + index + '. failed.')    
 
-
-if succesful == nr:
+if succesful == end - start + 1:
     print('[DONE] Every test was succesful.')
 else:
     print('[DONE] Task failed succesfully.')
