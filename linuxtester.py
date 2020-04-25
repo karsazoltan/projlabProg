dde69a7b3f43677cb0da2b80f7b1234ddaafd7d5
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2020-04-25 15:33:23 +0200

    Új a jar is valamiért? Meg a linuxtester frissítve

--- linuxtester.py
+++ linuxtester.py
@@ -5,10 +5,6 @@ import subprocess
 from subprocess import PIPE, STDOUT
 import time
 import sys
-from threading import Thread
-import threading
-
-javapath = "/usr/lib/jvm/jdk-13.0.1/bin/java"   # Ez új, azért, mert nekem a default java a java8
 
 succesful = 0
 nr = 44
@@ -16,75 +12,113 @@ path = ''
 test_folder = 'tesztek'
 tests = os.listdir("tesztek/")
 full_path = os.path.abspath(os.getcwd())
-directories = []
 start = 0
 end = 42
 
-if len(sys.argv) == 3:
-    start = int(sys.argv[1]) - 1   # -1, mert az indexek eltolódnak (arg: 37 40, ekkor eddig lefutott a 38 - 41. teszt :P)
-    end = int(sys.argv[2]) - 1     # -1 itt is
-
-
-tests = sorted(tests, key = lambda x:int(x[:2]))
-for directory in tests:
-    directories.append(directory)
-
-
-for directory in directories[start:end + 1]:
-    index = directory[:2].strip(' ')
-    stdout_ouptut = directory[-1] == '1'
-
+# This function checks the out.txt file of a given test case.
+def check_save_file(directory, index):
     path = os.path.join(full_path, test_folder, directory, index + '-cmd.txt')
     cmd = open(path, 'r')
-    full_cmd = ''
-    p = subprocess.Popen([javapath, '-jar', 'projlabProg.jar'], stdin=PIPE, stdout=PIPE, stderr=STDOUT)   # Itt a commandot listként adom meg
+    p = subprocess.Popen(['java','-jar', 'projlabProg.jar'], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
     for command in cmd.readlines():
         if 'load' in command:
-            command = 'load ' + os.path.join(test_folder, directory, index + '-in.txt\n')  # Itt meg a savenél a parancs végére teszek \n-t
+            command = 'load ' + os.path.join(test_folder, directory, index + '-in.txt\n')         # Ez változott az előzőhöz képest
         elif 'save' in command:
-            command = 'save ' + os.path.join(test_folder, directory, index + '-out.txt\n') # \n
+            command = 'save ' + os.path.join(test_folder, directory, index + '-out.txt\n')         # Ez változott az előzőhöz képest
             p.communicate(bytes(command, 'utf-8'))
             break
-        p.stdin.write(bytes(command, 'utf-8'))  # Viszont itt cserébe *nem* teszek \n-t a parancs végére (valszeg a cmd.readlines() \n-nel együtt ad sort)
+        p.stdin.write(bytes(command, 'utf-8'))
     cmd.close()
-
-
-
 
     okay = True
     ep = os.path.join(full_path, test_folder, directory, index + '-expected.txt')
     gp = os.path.join(full_path, test_folder, directory, index + '-out.txt')
 
-    try:                                                                    # try blokk, hogy ne dobja el magát print parancsra
-        with open(ep, 'r') as expected, open(gp, 'r') as generated:    
-            expected_lines = [line for line in expected]
-            generated_lines = [line for line in generated]
+    with open(ep, 'r') as expected, open(gp, 'r') as generated:    
+        expected_lines = [line for line in expected]
+        generated_lines = [line for line in generated]
 
-            if len(expected_lines) != len(generated_lines):
-                okay = False
-            
-            if okay:
-                for e, g in zip(expected_lines, generated_lines):
-                    e = e.strip('\n').replace(' ', '')
-                    g = g.strip('\n').replace(' ', '')
-                    if sorted(e) != sorted(g):
-                        print(e)
-                        print(g)
-                        okay = False
-                        break
-    except FileNotFoundError:                                               # catch blokk, azért el nem fogadjuk
-        print('Nincs output file: ' + index)
-        okay = False
+        if len(expected_lines) != len(generated_lines):
+            okay = False
+        if okay:
+            for e, g in zip(expected_lines, generated_lines):
+                e = e.strip('\n').replace(' ', '')
+                g = g.strip('\n').replace(' ', '')
+                if sorted(e) != sorted(g):
+                    print(e)
+                    print(g)
+                    okay = False
+                    break
+    return okay
 
 
-    if okay:
-        succesful += 1
-        print('[INFO] Test ' + index + '. succeeded.')
+
+
+# This function checks the standard output of a given test case.
+def check_stdout(directory, index):
+    path = os.path.join(full_path, test_folder, directory, index + '-cmd.txt')
+    ep = os.path.join(full_path, test_folder, directory, index + '-expected.txt')
+    expected = open(ep, 'r')
+    expected_lines = [line for line in expected]
+    
+    cmd = open(path, 'r')
+    p = subprocess.Popen(['java','-jar', 'projlabProg.jar'], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    for command in cmd.readlines():
+        if 'load' in command:
+            command = 'load ' + os.path.join(test_folder, directory, index + '-in.txt\n')         # Ez változott az előzőhöz képest
+        p.stdin.write(bytes(command, 'utf-8'))
+    stdout = p.communicate()[0].decode("utf-8").split('\n')
+    cmd.close()
+    expected.close()
+
+    problematic_chars = [' ', '\n', '\r', ',', '>', '.', '!', "'", ':']
+    for pc in problematic_chars:
+        stdout = [x.replace(pc, '') for x in stdout]
+        expected_lines = [x.replace(pc, '') for x in expected_lines]
+    
+    inside = 0
+    for el in expected_lines:
+        if el in stdout:
+            inside += 1
+    
+    if inside == len(expected_lines):
+        return True 
     else:
-        print('[WARNING] Test ' + index + '. failed.')    
+        return False
 
-if succesful == end - start + 1:
-    print('[DONE] Every test was succesful.')
-else:
-    print('[DONE] Task failed succesfully.')
-    print('Successful: ' + str(succesful) + '/' + str(end - start + 1))   # Csak mert miért is ne?
+# Main function that iterates on the given test cases.
+def main():
+    global start, end, tests, succesful
+    if len(sys.argv) == 3:
+        start = int(sys.argv[1]) - 1           # Ez változott az előzőhöz képest
+        end = int(sys.argv[2]) - 1             # Ez változott az előzőhöz képest
+    
+    directories = []
+    tests = sorted(tests, key = lambda x:int(x[:2]))
+    for directory in tests:
+        directories.append(directory)
+
+    for directory in directories[start:end + 1]:
+        index = directory[:2].strip(' ')
+        stdout_ouptut = directory[-1] == '1'
+
+        if not stdout_ouptut:
+            okay = check_save_file(directory, index)
+        else:
+            okay = check_stdout(directory, index)
+        
+
+        if okay:
+            succesful += 1
+            print('[INFO] Test ' + index + '. succeeded.')
+        else:
+            print('[WARNING] Test ' + index + '. failed.')
+
+    if succesful == end - start + 1:
+        print('[DONE] Every test was succesful.')
+    else:
+        print('[DONE] Task failed succesfully.')
+        print('Successful: ' + str(succesful) + '/' + str(end - start + 1))   # Csak mert miért is ne?
+
+
+main()
