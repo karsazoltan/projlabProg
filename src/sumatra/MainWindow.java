d2c6c5ccdca2f7a826d5f9684b4890a2d0e9dcfe
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2020-05-05 16:35:59 +0200

    Jupíí swing yay

---
+++ src/sumatra/MainWindow.java
@@ -0,0 +1,12 @@
+package sumatra;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        setSize(600, 600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
