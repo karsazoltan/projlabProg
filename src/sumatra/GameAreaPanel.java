5997c65554e892abf739677bc447b761fe6d25a1
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2020-05-06 12:52:33 +0200

    GAP saveLayout

--- src/sumatra/GameAreaPanel.java
+++ src/sumatra/GameAreaPanel.java
@@ -1,7 +1,41 @@
 package sumatra;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class GameAreaPanel extends JPanel {
-    // TODO STUB
+    private static GameAreaPanel instance = new GameAreaPanel();
+    public static GameAreaPanel getInstance() { return instance; }
+
+    private static ArrayList<TileView> tiles;
+
+    private GameAreaPanel() {
+        tiles = new ArrayList<>();
+    }
+
+    public static void saveLayout(String filename) throws IOException {
+        FileOutputStream fos = new FileOutputStream(filename);
+        PrintWriter pw = new PrintWriter(fos);
+        pw.write("datafile " + filename + ".data.txt\n");
+        pw.write("tiles " + tiles.size() + "\n");
+        for (int i = 0; i < tiles.size(); i++) {
+            Point p = tiles.get(i).getLayout();
+            pw.write("tile " + i + " " + p.x + " " + p.y + "\n");
+        }
+        pw.flush();
+        pw.close();
+    }
+
+    public static void attachTileViews() {
+
+    }
+
+    public static void loadTileViewsFromFile(String path) {
+
+    }
 }
---
+++ src/sumatra/TileView.java
@@ -0,0 +1,11 @@
+package sumatra;
+
+import java.awt.*;
+
+public class TileView {
+    // TODO STUB
+
+    public Point getLayout() {
+        return new Point(-1, -1);
+    }
+}
