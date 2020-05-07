0e7380b784f0048a791777666646065d64625c0c
Kacso Peter <kacso.pepe@yahoo.com>
2020-05-07 15:32:37 +0300

    Creature-be új függvények.

--- src/graphics/CreaturesPanel.java
+++ src/graphics/CreaturesPanel.java
@@ -12,18 +12,25 @@ import java.util.ArrayList;
 import sumatra.Creature;
 
 public class CreaturesPanel extends JPanel {
+    JPanel panel = new JPanel();
     public CreaturesPanel(){        
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        
         JLabel cLabel = new JLabel("Creatures");
         Font labelFont = cLabel.getFont();
         cLabel.setFont(new Font(labelFont.getName(), Font.PLAIN, 20));
         add( cLabel);
+        
+        JScrollPane scrollPane = new JScrollPane(panel);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        add( panel );
     }
 
     public void attachViews(){
         ArrayList<Creature> creatures = World.getInstance().getCreatures();
         for( Creature c : creatures ){
             OneCreaturePanel ocp = new OneCreaturePanel(c);
-            add( ocp );
+            panel.add( ocp );
             // do view thing too here TODO
         }
     }
--- src/graphics/OneCreaturePanel.java
+++ src/graphics/OneCreaturePanel.java
@@ -1,14 +1,29 @@
 package graphics;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+
+import java.awt.Font;
+import java.util.ArrayList;
 
 import sumatra.Creature;
 
 public class OneCreaturePanel extends JPanel implements IView{
-    
+    JLabel hpLabel = new JLabel();
+    JLabel manaLabel = new JLabel();
+    ArrayList<JLabel> iLabels = new ArrayList<JLabel>();
+    Creature creature;
 
-    public OneCreaturePanel(Creature creature){
-        // TODO Isten basszon swinget.
+    public OneCreaturePanel(Creature pcreature){
+        creature = pcreature;
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+        JLabel nameLabel = new JLabel(creature.getType() + creature.getIndex());
+        Font labelFont = nameLabel.getFont();
+        nameLabel.setFont(new Font(labelFont.getName(), Font.BOLD, 20));
+        add(nameLabel);
+        add(hpLabel);
+        add(manaLabel);
+        subjectChanged();
     }
 
     public void subjectChanged(){
