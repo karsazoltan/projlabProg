e922abdf1d6050caac7f3c6ac0ad1989d1607c04
jakdan99 <jakdan99@gmail.com>
2020-05-09 16:07:41 +0200

    Medve chooser

--- src/graphics/BearTileChooserWindow.java
+++ src/graphics/BearTileChooserWindow.java
@@ -1,17 +1,42 @@
 package graphics;
 
+import sumatra.Interpreter;
 import sumatra.Tile;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class BearTileChooserWindow extends JFrame {
-    // TODO Stub
+    int where;
 
     public BearTileChooserWindow(Tile t) {
-        // FEL TUDOD HASZNÁLNI VALID SZOMSZÉDOKHOZ
+        setSize(200, 90);
+        setTitle("Hova mozogjon a medve?");
+        setLayout(new GridLayout(0, 1));
+
+        JButton okButton = new JButton("Ok");
+        Component generic;
+        ActionListener lambda;
+
+        ArrayList<Integer> optionsList = new ArrayList<>();
+        for (Tile tile : t.getNeighbors()) {
+            optionsList.add(tile.getID());
+        }
+        final Integer[] options = (Integer[])(optionsList.toArray());
+
+        JComboBox<Integer> jcb = new JComboBox<Integer>(options);
+        generic = jcb;
+        lambda = (arg) -> {
+            int index = jcb.getSelectedIndex();
+            if (index < 0) return;
+            where = options[index];
+            dispose();
+        };
     }
 
     public int showDialog() {
-        return -1; // MELYIK TILE-RA MOZOGJON
+        return where; // MELYIK TILE-RA MOZOGJON
     }
 }
